7116177aa1bad1cb5626e55f3556860b408bdb6a
oleosb <oleosbo@gmail.com>
2023-08-23 19:04:45 -0300

    Configure Redux

--- src/Hooks/useFetch.js
+++ src/Hooks/useFetch.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 const useFetch = () => {
   const [data, setData] = React.useState(null);
   const [error, setError] = React.useState(null);
-  const [loading, setLoading] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
 
   const request = React.useCallback(async (url, options) => {
     let response;
--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -1,30 +1,29 @@
-import React from "react";
+import React from 'react';
 
 const types = {
   email: {
-    regex:
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: "Preencha um email válido",
+    regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: 'Preencha um email válido',
   },
   password: {
     regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
     message:
-      "A senha precisa ter 1 caracter maiúsculo, 1 minúsculo  e digito. Com no mínimo 8 caracteres.",
+      'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
   },
-  number:{
+  number: {
     regex: /^\d+$/,
-    message: 'Utilize apenas números'
-  }
+    message: 'Utilize números apenas.',
+  },
 };
 
 const useForm = (type) => {
-  const [value, setValue] = React.useState("");
+  const [value, setValue] = React.useState('');
   const [error, setError] = React.useState(null);
 
-  function validade(value) {
+  function validate(value) {
     if (type === false) return true;
     if (value.length === 0) {
-      setError("Preencha um  valor.");
+      setError('Preencha um valor.');
       return false;
     } else if (types[type] && !types[type].regex.test(value)) {
       setError(types[type].message);
@@ -36,7 +35,7 @@ const useForm = (type) => {
   }
 
   function onChange({ target }) {
-    if (error) validade(target.value);
+    if (error) validate(target.value);
     setValue(target.value);
   }
 
@@ -44,9 +43,9 @@ const useForm = (type) => {
     value,
     setValue,
     onChange,
-    validade: () => validade(value),
-    onBlur: () => validade(value),
     error,
+    validate: () => validate(value),
+    onBlur: () => validate(value),
   };
 };
 
