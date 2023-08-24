13538a444b8f70c680337795f73bddb2f6af263b
oleosb <oleosbo@gmail.com>
2023-08-23 21:27:22 -0300

    Photo Post

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -4,13 +4,14 @@ import {
   getDefaultMiddleware,
 } from "@reduxjs/toolkit";
 import photo from "./photo";
+import photoPost from "./photoPost";
 import token from "./token";
 import user from "./user";
 import feed from "./feed";
 import ui from "./ui";
 
 const middleware = [...getDefaultMiddleware()];
-const reducer = combineReducers({ photo, token, user, feed, ui });
+const reducer = combineReducers({ photo, photoPost, token, user, feed, ui });
 const store = configureStore({ reducer, middleware });
 
 export default store;
